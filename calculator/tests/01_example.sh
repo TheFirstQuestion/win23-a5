c19d5534cbb07bddbfa0b6ddcb0c4e156a04a93e
Steven G. Opferman <steven.g.opferman@gmail.com>
2024-02-26 20:05:50 -0800

    added test to CI file

--- calculator/tests/01_example.sh
+++ calculator/tests/01_example.sh
@@ -11,21 +11,26 @@ echo "To end a test early as a success, you should use the exit 0 command."
 
 echo "Invoke your program with the \$CALCULATOR variable; an example is below"
 
-
 # Test 01: Ensure the program runs without error with a simple, valid invocation.
-if ! $CALCULATOR 1 + 1; then  # If the return code of $PROGRAM is non-zero (i.e. error)...
+if ! $CALCULATOR 1 + 1; then # If the return code of $PROGRAM is non-zero (i.e. error)...
   echo 'ERROR! A valid run of the calculator (1 + 1) failed!'
   exit 1
 fi
 
 # Test 02: Ensure simple case has correct output...
-if [[ $($CALCULATOR 1 + 1) -ne 2 ]]; then  # If the output of the program is not 2...
+if [[ $($CALCULATOR 1 + 1) -ne 2 ]]; then # If the output of the program is not 2...
   echo 'ERROR! A valid run of the calculator (1 + 1) failed to produce 2 as an output!'
   exit 1
 fi
 
 # Test 03: Ensure program errors with an invalid operand
-if $CALCULATOR 3 @ 2; then  # If the return code of $PROGRAM is zero (i.e. success)...
+if $CALCULATOR 3 @ 2; then # If the return code of $PROGRAM is zero (i.e. success)...
   echo 'ERROR! An invalid run of the application (3 @ 2) apparently succeeded?!'
+  exit 1
+fi
+
+# Test 04: Another correct case
+if [[ $($CALCULATOR 6 '*' 6) -ne 36 ]]; then
+  echo 'ERROR! A valid run of the calculator (6 * 6) failed to produce 36 as an output!'
   exit 1
 fi
